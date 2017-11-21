29f28b1e8319e7e054ce3fe0d869bc2277d90465
doone <doone@doone-PC>
2017-11-21 14:50:39 +0800

    java7和java8对List排序的语法

--- src/com/zfw/java8/list/TestListSort.java
+++ src/com/zfw/java8/list/TestListSort.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestList {
+public class TestListSort {
 
 	private List<String> name1;
 	private List<Integer> name2;
